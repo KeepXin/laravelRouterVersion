effb52b3c1202c3a95e6bf6c1ce56385da76617e
clz <941803830@qq.com>
2018-11-15 08:43:38 +0800

    init

---
+++ LumenRoutesRequests.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/11/15
+ * Time: 8:23
+ */
+namespace App;
+use Laravel\Lumen\Concerns\RoutesRequests;
+
+use Exception;
+use Throwable;
+use Laravel\Lumen\Http\Request as LumenRequest;
+
+
+trait LumenRoutesRequests
+{
+    use RoutesRequests;
+
+    /**
+     * @param null $request
+     * @return \Illuminate\Http\Response|mixed
+     */
+    public function dispatch($request = null)
+    {
+        list($method, $pathInfo) = $this->parseIncomingRequest($request);
+
+        $version = $this->getVersion($request);
+        $router = $this->router->getRoutes();
+        if(!isset($router[$version]) || !$router[$version]){
+            throw new \RuntimeException('unknown route version');
+        }
+        $router = $router[$version];
+
+        try {
+            $this->boot();
+
+            return $this->sendThroughPipeline($this->middleware, function () use ($method, $pathInfo,$router) {
+                if (isset($router[$method . $pathInfo])) {
+                    return $this->handleFoundRoute([true, $router[$method . $pathInfo]['action'], []]);
+                }
+                return $this->handleDispatcherResponse(
+                    $this->createDispatcher()->dispatch($method, $pathInfo)
+                );
+            });
+        } catch (Exception $e) {
+            return $this->prepareResponse($this->sendExceptionToHandler($e));
+        } catch (Throwable $e) {
+            return $this->prepareResponse($this->sendExceptionToHandler($e));
+        }
+    }
+
+    public function getVersion($request){
+        if (! $request) {
+            $request = LumenRequest::capture();
+        }
+
+        return  $request->header('version', 'v1');
+    }
+
+
+}
